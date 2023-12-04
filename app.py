c700ecf40a9c336ed566da76bbc9beaf3fe4748b
Pratik Dwivedi <93965493+Dekode1859@users.noreply.github.com>
2023-12-04 12:34:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,40 @@
 import streamlit as st
 
 def main():
-    st.title("Software Developer Portfolio")
+    st.title("Hey there!")
+    st.subheader("Welcome to my portfolio website!")
+    st.text("Feel free to browse around!")
     
-    col1, cols, colss, col2, col3 = st.columns(5)
+    st.header("About Me")
+    st.text("I am a data scientist with a passion for machine learning and deep learning.")
+    st.text("I am also a full stack developer with experience in Python, JavaScript, and SQL.")
+    st.text("I am currently looking for a full time position in data science or software engineering.")
     
-    with col1:
-        # make a clickable button named home that reroutes to the home page
-        if st.button("Home"):
-            st.write("Welcome to my portfolio!")
-            st.write("My name is Michael and I am a software developer.")
-            st.write("I am currently a senior at the University of Texas at Dallas majoring in Computer Science.")
-            st.write("I am currently looking for a full-time position as a software developer.")
-            st.write("I am proficient in Python, Java, C, and C++.")
-            st.write("I have experience in web development using HTML, CSS, and JavaScript.")
-            st.write("I have experience in SQL and NoSQL databases.")
-            st.write("I have experience in cloud computing using AWS.")
-            st.write("I have experience in machine learning using Python and TensorFlow.")
-            st.write("I have experience in data analysis using Python and Pandas.")
-            st.write("I have experience in mobile development using Android Studio.")
-            st.write("I have experience in game development using Unity.")
-            st.write("I have experience in version control using Git.")
-            st.write("I have experience in agile development using Scrum.")
-            st.write("I have experience in software testing.")
-            
-    with col2:
-        # make a clickable button named projects that reroutes to the projects page
-        if st.button("Projects"):
-            st.write("Here are some of my projects!")
-            st.write("My projects are also available on my GitHub profile.")
-            st.write("  ")
-            st.write("  ")  
-            st.write("  ")
-            
-            # make a clickable button named project1 that reroutes to the project1 page
-            if st.button("Project 1"):
-                st.write("Project 1")
-                st.write("  ")
-                st.write("  ")
-                st.write("  ")
-                
-            # make a clickable button named project2 that reroutes to the project2 page
-            if st.button("Project 2"):
-                st.write("Project 2")
-                st.write("  ")
-                st.write("  ")
-                st.write("  ")
-                
-            # make a clickable button named project3 that reroutes to the project3 page
-            if st.button("Project 3"):
-                st.write("Project 3")
-                st.write("  ")
-                st.write("  ")
-                st.write("  ")
-                
+    st.header("My Projects")
+    st.text("Here are some of my projects:")
+    st.text("1. [Project 1](    )")
+    st.text("2. [Project 2](    )")
+    st.text("3. [Project 3](    )")
+    
+    st.header("Contact Me")
+    st.text("Email: [email](mailto:    )")
+    st.text("LinkedIn: [LinkedIn](    )")
+    st.text("GitHub: [GitHub](    )")
+    st.text("Twitter: [Twitter](    )")
+    st.text("Instagram: [Instagram](    )")
+    st.text("Facebook: [Facebook](    )")
+    st.text("YouTube: [YouTube](    )")
+    st.text("Twitch: [Twitch](    )")
+    st.text("Reddit: [Reddit](    )")
+    st.text("Pinterest: [Pinterest](    )")
+    st.text("Tumblr: [Tumblr](    )")
+    st.text("Snapchat: [Snapchat](    )")
+    st.text("WhatsApp: [WhatsApp](    )")
+    st.text("Telegram: [Telegram](    )")
+    st.text("WeChat: [WeChat](    )")
+
+                    
+    
     
 
 if __name__ == "__main__":
