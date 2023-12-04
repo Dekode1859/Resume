5bf4834c8a911dc733042b0880834e7138a3e087
Pratik Dwivedi <93965493+Dekode1859@users.noreply.github.com>
2023-12-04 12:31:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,40 +6,55 @@ def main():
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        st.write("Home")
-        
+        # make a clickable button named home that reroutes to the home page
+        if st.button("Home"):
+            st.write("Welcome to my portfolio!")
+            st.write("My name is Michael and I am a software developer.")
+            st.write("I am currently a senior at the University of Texas at Dallas majoring in Computer Science.")
+            st.write("I am currently looking for a full-time position as a software developer.")
+            st.write("I am proficient in Python, Java, C, and C++.")
+            st.write("I have experience in web development using HTML, CSS, and JavaScript.")
+            st.write("I have experience in SQL and NoSQL databases.")
+            st.write("I have experience in cloud computing using AWS.")
+            st.write("I have experience in machine learning using Python and TensorFlow.")
+            st.write("I have experience in data analysis using Python and Pandas.")
+            st.write("I have experience in mobile development using Android Studio.")
+            st.write("I have experience in game development using Unity.")
+            st.write("I have experience in version control using Git.")
+            st.write("I have experience in agile development using Scrum.")
+            st.write("I have experience in software testing.")
+            
     with col2:
-        st.write("Project Showcase")
-        
-    with col3:
-        st.write("About")
+        # make a clickable button named projects that reroutes to the projects page
+        if st.button("Projects"):
+            st.write("Here are some of my projects!")
+            st.write("My projects are also available on my GitHub profile.")
+            st.write("  ")
+            st.write("  ")  
+            st.write("  ")
+            
+            # make a clickable button named project1 that reroutes to the project1 page
+            if st.button("Project 1"):
+                st.write("Project 1")
+                st.write("  ")
+                st.write("  ")
+                st.write("  ")
+                
+            # make a clickable button named project2 that reroutes to the project2 page
+            if st.button("Project 2"):
+                st.write("Project 2")
+                st.write("  ")
+                st.write("  ")
+                st.write("  ")
+                
+            # make a clickable button named project3 that reroutes to the project3 page
+            if st.button("Project 3"):
+                st.write("Project 3")
+                st.write("  ")
+                st.write("  ")
+                st.write("  ")
+                
     
-    
-    
-
-def show_home_page():
-    st.header("Home")
-    
-    # Add your home page content here
-    st.write("Welcome to my portfolio!")
-    st.write("I'm a software developer.")
-    st.write("I like to build cool stuff with Python and JavaScript.")
-
-def show_project_showcase():
-    st.header("Project Showcase")
-    
-    # Add your project showcase content here
-    st.write("Project 1")
-    st.write("Project 2")
-    st.write("Project 3")
-
-def show_about_page():
-    st.header("About")
-    
-    # Add your about page content here
-    st.write("Name: John Doe")
-    st.write("Experience: X years")
-    st.write("Skills: Python, JavaScript, HTML, CSS")
 
 if __name__ == "__main__":
     main()
